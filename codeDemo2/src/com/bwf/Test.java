6df2521adc9981d220249a1ffcc8bbf7bf7e4508
bwfadmin <bwfadmin@bwfadmin-PC>
2017-06-20 19:12:24 +0800

    aaa

---
+++ codeDemo2/src/com/bwf/Test.java
@@ -0,0 +1,11 @@
+package com.bwf;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("----111111----");
+		System.out.println("---222aaa-------");
+		System.out.println("3333333333333");
+		System.out.println("4444444444444");
+		System.out.println("4444444444444aaa");
+	}
+}
